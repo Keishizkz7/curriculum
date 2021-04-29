b263bc3e2bb6bb426b1445531b6e4825d14e6933
Keishizkz7 <78611433+Keishizkz7@users.noreply.github.com>
2021-04-29 21:54:00 +0900

    課題の提出
    配列、連想配列の作成とIT基本用語の意味

---
+++ 2-7/2-7.php
@@ -0,0 +1,10 @@
+<?php
+$deta = array("red"=>"赤","blue"=>"青","green"=>"緑"); 
+var_dump($deta);
+
+echo "<br>";
+
+$deta1 = array("red"=>"赤","blue"=>"青","green"=>"緑","yellow"=>"黄"); 
+var_dump($deta1);
+
+?>
