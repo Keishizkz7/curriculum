05a5d96d8882def920d0ed6c1e417181860d7ab0
Keishizkz7 <78611433+Keishizkz7@users.noreply.github.com>
2021-05-06 14:48:39 +0900

    課題の提出
    直方体の体積を求める関数の作成

---
+++ 2-10/2-10.php
@@ -0,0 +1,12 @@
+<?php
+
+function chokuho($base,$width,$height) {
+    $x = $base * $width * $height;
+    print "直方体の体積は".$x."です。";
+}
+
+chokuho(5,10,8);
+
+//直方体の体積を求める関数を作成してください。
+//その関数を使用し、縦=5cm、横=10cm、高さ=8cmの直方体の体積を求めてください。
+?>
