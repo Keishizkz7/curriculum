7d104fa71892bc9277ff08b8abc3fe616afe14db
Keishizkz7 <78611433+Keishizkz7@users.noreply.github.com>
2021-08-23 22:39:39 +0900

    課題の提出
    PHP3章 バグ修正を行う
    test1・test2を作成

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+// セッション開始
+session_start();
+
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "root";
+// データベース名
+$db['dbname'] = "YIGroupBlog";
+
+$db['host'] = "mysql:host=localhost;charset=utf8;dbname=YIGroupBlog";
+
+?>
